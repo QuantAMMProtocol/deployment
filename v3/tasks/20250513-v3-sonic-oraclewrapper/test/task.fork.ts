e463dde134f7c61accd679bae3bc4323c7954134
christian harrington <christian@bulkbit.systems>
2025-06-03 15:12:19 +0100

    test name change

--- v3/tasks/20250513-v3-sonic-oraclewrapper/test/task.fork.ts
+++ v3/tasks/20250513-v3-sonic-oraclewrapper/test/task.fork.ts
@@ -4,7 +4,7 @@ import { describeForkTest, getForkedNetwork, Task, TaskMode } from '@src';
 describeForkTest('QuantAMMPool', 'sepolia', 8140847, function () {
   let task: Task;
 
-  const TASK_NAME = '20250513-v3-aero-oraclewrapper';
+  const TASK_NAME = '20250513-v3-sonic-oraclewrapper';
 
   before('run task', async () => {
     task = new Task(TASK_NAME, TaskMode.TEST, getForkedNetwork(hre));
